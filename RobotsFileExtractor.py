847ddf443035686ebb0f624c0ba3eca03c634699
DzasterAbz <abhiramonmail@gmail.com>
2020-04-18 14:05:50 -0400

    Added Scripts - UNORDERED

--- RobotsFileExtractor.py
+++ RobotsFileExtractor.py
@@ -9,4 +9,6 @@ def getRobotsTXT(webURL):
     
     request = urllib.request.urlopen(URLPath + "robots.txt", dataContent = None)
     dataContent = io.TextIOWrapper(request, encoding="UTF-8")
-    
+    return dataContent
+
+print(getRobotsTXT("https://google.com"))
